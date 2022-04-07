ba50ead68938800a26dbfe126a2b1e708f1b0099
wzd-hash <1633993057@qq.com>
2022-04-07 18:46:19 +0800

    修改测试代码

--- src/main/java/com/test/helloworld/HelloWorldApplication.java
+++ src/main/java/com/test/helloworld/HelloWorldApplication.java
@@ -3,12 +3,16 @@ package com.test.helloworld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @SpringBootApplication
 public class HelloWorldApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldApplication.class, args);
-        System.out.println("hello world");
+        System.out.println("hello world"+
+                new SimpleDateFormat("yyyy-HH-mm HH:mm:ss").format(new Date()));
     }
 
 }
