71d52a679e132b41e9c783554689a1c1f51b44d7
wzd-hash <1633993057@qq.com>
2022-04-07 15:42:48 +0800

    测试

---
+++ src/main/java/com/test/helloworld/HelloWorldApplication.java
@@ -0,0 +1,14 @@
+package com.test.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
+        System.out.println("hello world");
+    }
+
+}
